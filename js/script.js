5eb92aed8f79e532286a06bb8b172d6a7275d193
DanielZamperlini <monkeydluffysan2021@gmail.com>
2021-12-11 19:42:43 -0300

    bicicletas js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+//links do menu
 const links = document.querySelectorAll(".header-menu a");
 
 function ativarLink(link) {
@@ -9,3 +10,16 @@ function ativarLink(link) {
 }
 
 links.forEach(ativarLink);
+
+// ativar items do orçamento
+
+const parametros = new URLSearchParams(location.search);
+
+function ativarProduto(parametro) {
+  const elemento = document.getElementById(parametro);
+  if (elemento) {
+    elemento.checked = true;
+  }
+}
+
+parametros.forEach(ativarProduto);
